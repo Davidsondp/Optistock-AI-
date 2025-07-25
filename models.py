350a71ad75b286772dcfabe4bfde66e0a9010f9c
Davidsondp <technologyinte@gmail.com>
2025-07-25 12:11:18 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,13 +1,10 @@
-from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from models import db
 
-db = SQLAlchemy()
-
-class User(db.Model):
+class Salida(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(256), nullable=False)
-
-class Producto(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100), nullable=False)
+    producto_id = db.Column(db.Integer, db.ForeignKey('producto.id'), nullable=False)
     cantidad = db.Column(db.Integer, nullable=False)
+    fecha = db.Column(db.DateTime, default=datetime.utcnow)
+
+    producto = db.relationship('Producto', backref=db.backref('salidas', lazy=True))
