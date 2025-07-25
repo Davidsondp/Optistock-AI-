c9bf74994b07357e9003646bcadb86dee525165e
Davidsondp <technologyinte@gmail.com>
2025-07-24 23:20:18 -0300

    Update models.py

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(256), nullable=False)
 
 class Producto(db.Model):
     id = db.Column(db.Integer, primary_key=True)
