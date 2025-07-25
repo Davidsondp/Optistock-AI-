3cdc360d5566d87bb2edec06796159a61eba69a4
Davidsondp <technologyinte@gmail.com>
2025-07-25 12:56:12 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 from models import db
 
-class Salida(db.Model):
+class Movimiento(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     producto_id = db.Column(db.Integer, db.ForeignKey('producto.id'), nullable=False)
+    tipo = db.Column(db.String(10), nullable=False)  # 'entrada' o 'salida'
     cantidad = db.Column(db.Integer, nullable=False)
-    fecha = db.Column(db.DateTime, default=datetime.utcnow)
+    fecha = db.Column(db.Date, nullable=False, default=datetime.utcnow)
 
-    producto = db.relationship('Producto', backref=db.backref('salidas', lazy=True))
+    producto = db.relationship('Producto', backref=db.backref('Movimiento', lazy=True))
