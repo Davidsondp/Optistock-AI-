d184904fbc87b9a487e89c9a2fa15a8f8f96824c
Davidsondp <technologyinte@gmail.com>
2025-07-26 18:43:28 -0300

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,7 +1,6 @@
 from app import app
 from models import db
 
-# Crea las tablas dentro del contexto de la app Flask
 with app.app_context():
     db.create_all()
-    print("✅ Tablas creadas exitosamente.")
+    print(" Tablas creadas correctamente.")
