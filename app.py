cc4f43f2a41c4c6e694b454ae0634a3260676fc8
Davidsondp <technologyinte@gmail.com>
2025-07-26 20:15:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -202,4 +202,13 @@ def logout():
 if __name__ == "__main__":
     app.run(debug=True)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template("500.html"), 500
+
+
 
